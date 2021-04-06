5c0cce97555271a373d40014318d85a39c1c4501
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2021-04-06 21:43:42 +0800

    publish

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Fruit : MonoBehaviour
+{
+    public int score = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    public Text Countdown;
+    public Text PlayTime;
+    public float Second = 3f;
+    private float sec;
+    public float PlaySecond = 5f;
+
+    void Start(){
+        sec = Second + 2;
+        PlayTime.text = ((int)PlaySecond).ToString();
+        StartPrepareCountDown();
+    }
+
+    void Update()
+    {
+    }
+
+    void Prepare()
+    {
+        if(sec > 0){
+            if(sec < Second + 1){
+                Countdown.text = ((int)sec).ToString();
+            }
+            sec -= 1;
+            return;
+        }
+
+        Countdown.enabled  = false;
+        StartPlayCountDown();
+        CancelInvoke("Prepare");
+    }
+
+    void StartPrepareCountDown()
+    {
+        InvokeRepeating("Prepare", 0, 1);
+    }
+
+    void StartPlayCountDown(){
+        InvokeRepeating("PlayCountDown", 1, 1);
+    }
+
+    void PlayCountDown(){
+        if(PlaySecond > 0){
+            PlaySecond -= 1;
+            PlayTime.text = ((int)PlaySecond).ToString();
+            return;
+        }
+        CancelInvoke("PlayCountDown");
+    }
+
+    void GameStart(){
+        Debug.Log("game start");
+    }
+}
